d2f948342c4b4eee56e45af028d1464dff753ba4
ziuge <xwcjwwd@ewhain.net>
2022-07-09 20:41:42 +0900

    LEDBoard Return key end editing

--- LEDBoard/AppDelegate.swift
+++ LEDBoard/AppDelegate.swift
@@ -13,18 +13,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        sleep(1)
+        
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Youtube - 사용자가 프리미엄 결제를 했으면 play, 일시정지
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Youtube - 결제하지 않은 사람들에게 팝업창을 띄우도록
+        // 카카오톡 - 잠금 화면 다시 띄우기
+        // 금융앱 - 백그라운드에 있을 때 화면 안보이도록
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available (iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
+    @available (iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
---
+++ LEDBoard/BoardViewController.swift
@@ -0,0 +1,85 @@
+//
+//  BoardViewController.swift
+//  LEDBoard
+//
+//  Created by CHOI on 2022/07/06.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textColorButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet var buttonList: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        designButton(sendButton, buttonTitle: "전송", highlightedTitle: "보내!", bgColor: .cyan)
+        designButton(textColorButton, buttonTitle: "색상", highlightedTitle: "바꿔!", bgColor: .yellow)
+        designTextField()
+        
+        self.userTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+
+    func studyOutletCollection() {
+        // 1. 반복문
+        let buttonArray: [UIButton] = [sendButton, textColorButton]
+        
+        for item in buttonArray {
+            item.backgroundColor = .blue
+            item.layer.cornerRadius = 2
+        }
+        
+        // 2. 아웃렛
+        for item in buttonList {
+            item.backgroundColor = .blue
+            item.layer.cornerRadius = 2
+        }
+    }
+
+    // textField 디자인 바꾸기
+    func designTextField() {
+        userTextField.placeholder = "내용을 작성해주세요"
+        userTextField.text = "안녕하세요"
+        userTextField.keyboardType = .emailAddress
+        userTextField.font = .systemFont(ofSize: 14)
+        
+    }
+    
+    // buttonOutletVariableName: 외부 매개변수, Argument Label -> 생략 가능 (_ : 와일드 카드 식별자)
+    // buttonName: 내부 매개변수, Parameter Name
+    func designButton(_ buttonName: UIButton, buttonTitle: String, highlightedTitle: String, bgColor: UIColor) {
+        buttonName.setTitle(buttonTitle, for: .normal)
+        buttonName.setTitle(highlightedTitle, for: .highlighted)
+        buttonName.backgroundColor = bgColor
+        buttonName.layer.cornerRadius = 8
+        buttonName.layer.borderColor = UIColor.black.cgColor
+        buttonName.layer.borderWidth = 3
+        buttonName.setTitleColor(.red, for: .normal)
+        buttonName.setTitleColor(.blue, for: .highlighted)
+    }
+    
+
+    @IBAction func sendButtonClicked(_ sender: Any) {
+        print(userTextField.text?.count)
+        print(sendButton.currentTitle)
+        resultLabel.text = userTextField.text
+    }
+    
+    @IBAction func tapGestureClicked(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    // 연결 관계 다 끊어보고 Any 타입으로 연결, 기능이 같은 것 합치기
+    
+}
